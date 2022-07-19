cf8a1b0f2d28dc2794a30c849ca165d2d11a18f3
meeks484 <meeks484@gmail.com>
2022-07-18 23:23:03 -0600

    put the statics back on

--- src/main/java/MyStack/MyStack.java
+++ src/main/java/MyStack/MyStack.java
@@ -4,11 +4,11 @@ import java.lang.annotation.ElementType;
 import java.util.Arrays;
 
 public class MyStack  {
-    final int GROWTH_SIZE = 5;
-    int capacity;
-    int element[] = new int[1];
-    int elementCount = 0;
-    void push(int data){
+    static final int GROWTH_SIZE = 5;
+    static int capacity;
+    static int element[] = new int[1];
+    static int elementCount = 0;
+    static void push(int data){
         if (elementCount==element.length-1){
             capacity = element.length+GROWTH_SIZE;
             element= Arrays.copyOf(element,capacity);
@@ -19,14 +19,14 @@ public class MyStack  {
         System.out.println("Value "+data+" has been pushed");
     }
 
-    int pop(){
+    static int pop(){
         int topNumber = element[elementCount];
         element[elementCount] = 0;
         elementCount--;
         return topNumber;
     }
 
-    int peek(){
+    static int peek(){
         return element[elementCount];
     }
 
@@ -34,11 +34,11 @@ public class MyStack  {
     //
     //}
 
-    int size(){
+    static int size(){
         return elementCount;
     }
 
-    boolean search(int val){
+    static boolean search(int val){
         boolean found = false;
         for (int i = 0 ; i < elementCount ; i++){
             if (element[i]==val) {
@@ -51,7 +51,7 @@ public class MyStack  {
             return false;
         }
     }
-    public void main(String[] args) {
+    static public void main(String[] args) {
         push(25);
         push(15);
         push(10);
